b1cf0103eaddef233c09a934c07ba98dd776ca62
goodnighthsu <goodnighthsu@msn.com>
2014-02-14 12:18:20 +0800

    Init

---
+++ LXDataManager/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LXDataManager
+//
+//  Created by Leon on 14-2-13.
+//  Copyright (c) 2014年 Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LXDataManagerTests/LXDataManagerTests.m
@@ -0,0 +1,34 @@
+//
+//  LXDataManagerTests.m
+//  LXDataManagerTests
+//
+//  Created by Leon on 14-2-13.
+//  Copyright (c) 2014年 Leon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LXDataManagerTests : XCTestCase
+
+@end
+
+@implementation LXDataManagerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
